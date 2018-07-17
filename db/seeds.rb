66fab0f87cf72bdfd095f6df3df5aa96f85410fa
brickstar <brickstar@gmail.com>
2018-07-17 14:05:30 -0600

    seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,25 +10,19 @@ user  = User.create(username: 'Heidi', password: 'love', role: 0)
 CATEGORIES = ["Red", "Orange", "Green", "Yellow", "Blue", "Indigo", "Violet", "Pink"]
 IDEAS = ["Peace", "Love", "Understanding", "Haromony", "Music", "Friendship", "Family", "Community"]
 DESCRIPTION = ["Perfect Idea", "Wonderfull Idea", "Clever Idea", "Meaningful Idea", "Productive Idea", "Unique Idea", "First Idea", "Last Idea", "Beginning Idea", "Interesting Idea", "Smart Idea", "Genius Idea", "What an Idea!"]
-TITLES = ["Green Image", "Yellow Image", "Blue Image", "Red Image", "Purple Image", "White Image", "Pink Image", "Black Image"]
+URLS = ['https://robohash.org/1', 'https://robohash.org/a', 'https://robohash.org/b', 'https://robohash.org/e', 'https://robohash.org/c', 'https://robohash.org/d', 'https://robohash.org/f']
 
 
 CATEGORIES.each do |title|
   category = Category.create!(title: title)
   puts "Created #{category.title}"
-  10.times do |num|
-    category.ideas.create!(title: IDEAS.sample, description: DESCRIPTION.sample, user: admin)
-    Image.create(url: 'www.example.com', title: TITLES.sample)
-    puts "Created #{category.ideas[num].title}"
-  end
+  idea = category.ideas.create!(title: IDEAS.sample, description: DESCRIPTION.sample, user: admin)
+  idea.images.create!(url: URLS.sample, title: CATEGORIES.sample)
 end
 
 CATEGORIES.each do |title|
   category = Category.create!(title: title)
   puts "Created #{category.title}"
-  10.times do |num|
-    category.ideas.create!(title: IDEAS.sample, description: DESCRIPTION.sample, user: user)
-    Image.create(url: 'www.example.com', title: TITLES.sample)
-    puts "Created #{category.ideas[num].title}"
-  end
+  idea = category.ideas.create!(title: IDEAS.sample, description: DESCRIPTION.sample, user: user)
+  idea.images.create!(url: URLS.sample, title: CATEGORIES.sample)
 end
--- spec/features/user_can_see_ideas_spec.rb
+++ spec/features/user_can_see_ideas_spec.rb
@@ -2,9 +2,10 @@ require 'rails_helper'
 
 describe 'registered user visits their show page' do
   it 'should see all their ideas' do
-    user = User.create(username: 'heidi', password: 'love', role: 0)
-    idea1 = user.ideas.create(title: 'idea1', description: 'this is my first idea')
-    idea2 = user.ideas.create(title: 'idea2', description: 'this is my second idea')
+    user = User.create!(username: 'heidi', password: 'love', role: 0)
+    category = Category.create!(title: 'Green')
+    idea1 = user.ideas.create!(title: 'idea1', description: 'this is my first idea', category: category)
+    idea2 = user.ideas.create!(title: 'idea2', description: 'this is my second idea', category: category)
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
