7a2c5f19d5c106e7c4c5bcbfc5111a7ecc6e3d02
brickstar <brickstar@gmail.com>
2018-07-16 23:32:37 -0600

    seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,14 +4,15 @@ Idea.destroy_all
 CATEGORIES = ["Red", "Orange", "Green", "Yellow", "Blue", "Indigo", "Violet", "Pink"]
 IDEAS = ["Peace", "Love", "Understanding", "Haromony", "Music", "Friendship", "Family", "Community"]
 DESCRIPTION = ["Perfect Idea", "Wonderfull Idea", "Clever Idea", "Meaningful Idea", "Productive Idea", "Unique Idea", "First Idea", "Last Idea", "Beginning Idea", "Interesting Idea", "Smart Idea", "Genius Idea", "What an Idea!"]
-
+TITLES = ["Green Image", "Yellow Image", "Blue Image", "Red Image", "Purple Image", "White Image", "Pink Image", "Black Image"]
+URLS = []
 
 CATEGORIES.each do |title|
   category = Category.create!(title: title)
   puts "Created #{category.title}"
   10.times do |num|
     category.ideas.create!(title: IDEAS.sample, description: DESCRIPTION.sample)
-    Image.create(url: 'www.example.com', title: 'image')
+    Image.create(url: 'www.example.com', title: TITLES.sample)
     puts "Created #{category.ideas[num].title}"
   end
 end
