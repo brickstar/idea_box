1e17c98f140284747b933e4006f984406d4f0750
brickstar <brickstar@gmail.com>
2018-07-16 20:14:31 -0600

    user can login

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180717010814) do
+ActiveRecord::Schema.define(version: 20180717012733) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,15 @@ ActiveRecord::Schema.define(version: 20180717010814) do
   create_table "images", force: :cascade do |t|
     t.string "title"
     t.string "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "ideas", "categories"
---
+++ spec/features/registration_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+describe 'registration workflow' do
+  it 'submits a registration successfully' do
+    username = 'Pearl'
+
+    visit root_path
+
+    click_on 'Sign Up!'
+
+    expect(current_path).to eq(new_user_path)
+
+    fill_in :user_username, with: username
+    fill_in :user_password, with: 'sosecret'
+
+    click_on 'Create User'
+
+    expect(current_path).to eq(user_path(User.last))
+    expect(page).to have_content("Welcome, #{username}!")
+  end
+
+  it 'submits a registration form with duplicate username' do
+    skip
+    username = 'Pearl'
+    User.create!(username: username, password: 'sosecret')
+    count = User.count
+
+    visit root_path
+
+    click_on 'Sign Up!'
+
+    expect(current_path).to eq(new_user_path)
+
+    fill_in :user_username, with: username
+    fill_in :user_password, with: '12345'
+
+    click_on 'Create User'
+
+    expect(current_path).to eq(users_path)
+    expect(User.count). to eq(count)
+  end
+end
