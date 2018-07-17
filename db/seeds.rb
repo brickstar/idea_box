80e5dc94bfdb3e1356fdcc79060f60f982af6c46
brickstar <brickstar@gmail.com>
2018-07-17 10:56:37 -0600

    seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@ CATEGORIES = ["Red", "Orange", "Green", "Yellow", "Blue", "Indigo", "Violet", "P
 IDEAS = ["Peace", "Love", "Understanding", "Haromony", "Music", "Friendship", "Family", "Community"]
 DESCRIPTION = ["Perfect Idea", "Wonderfull Idea", "Clever Idea", "Meaningful Idea", "Productive Idea", "Unique Idea", "First Idea", "Last Idea", "Beginning Idea", "Interesting Idea", "Smart Idea", "Genius Idea", "What an Idea!"]
 TITLES = ["Green Image", "Yellow Image", "Blue Image", "Red Image", "Purple Image", "White Image", "Pink Image", "Black Image"]
-URLS = []
+
 
 CATEGORIES.each do |title|
   category = Category.create!(title: title)
