dd089e9b866d44516ba29d33199716469a94c61f
brickstar <brickstar@gmail.com>
2018-07-16 22:12:42 -0600

    user can login and logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,10 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.new(user_params)
-    if user.save
-      redirect_to user_path(user)
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
     else
       flash[:error] = 'Username already exists'
       redirect_to new_user_path
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root 'welcome#index'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get  'logout', to: 'sessions#destroy'
+
 
   resources :users, only: [:index, :new, :create, :show]
 end
