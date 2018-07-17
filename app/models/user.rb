674e7ba45a3f37719d88890e96c5982ca4500203
brickstar <brickstar@gmail.com>
2018-07-17 11:12:56 -0600

    added role to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   validates :username, uniqueness: true, presence: true
   validates_presence_of :password, require: true
 
-  has_many :ideas
+  has_many :ideas, dependent: :destroy
 
   has_secure_password
 end
