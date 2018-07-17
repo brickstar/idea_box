b9e775ad5804ee441cc95fb29c547537567ffe66
brickstar <brickstar@gmail.com>
2018-07-16 19:12:33 -0600

    image relationships tested

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,3 +1,6 @@
 class Image < ApplicationRecord
   validates_presence_of(:title, :url)
+
+  has_many :image_ideas
+  has_many :ideas, through: :image_ideas
 end
--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -5,4 +5,8 @@ describe Image, type: :model do
     it { should validate_presence_of :title }
     it { should validate_presence_of :url }
   end
+
+  describe 'relationships' do
+    it { should have_many(:ideas).through(:image_ideas)}
+  end
 end
