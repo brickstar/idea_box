5462a005c0f3c0358e1c592e8c87f1f2a25cfada
brickstar <brickstar@gmail.com>
2018-07-17 12:58:31 -0600

    user sees ideas spec

---
+++ spec/features/user_can_see_ideas_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe 'registered user visits their show page' do
+  it 'should see all their ideas' do
+    user = User.create(username: 'heidi', password: 'love', role: 0)
+    idea1 = user.ideas.create(title: 'idea1', desciption: 'this is my first idea')
+    idea2 = user.ideas.create(title: 'idea2', desciption: 'this is my second idea')
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit user_path(user)
+
+    expect(page).to have_content(idea1.title)
+    expect(page).to have_content(idea2.description)
+  end
+end
