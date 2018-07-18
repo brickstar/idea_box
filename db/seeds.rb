069e9f447de6b96846e629b8028c445a97a7220b
brickstar <brickstar@gmail.com>
2018-07-18 10:34:13 -0600

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,27 +6,32 @@ User.destroy_all
 admin = User.create(username: 'Pearl', password: 'love', role: 1)
 user  = User.create(username: 'Heidi', password: 'love', role: 0)
 
+IMAGEURLS = []
+1000.times do
+  IMAGEURLS << "https://robohash.org/#{rand(1000)}"
+end
 
-CATEGORIES = ["Red", "Orange", "Green", "Yellow", "Blue", "Indigo", "Violet", "Pink"]
-IDEAS = ["Peace", "Love", "Understanding", "Haromony", "Music", "Friendship", "Family", "Community"]
-DESCRIPTION = ["Perfect Idea", "Wonderfull Idea", "Clever Idea", "Meaningful Idea", "Productive Idea", "Unique Idea", "First Idea", "Last Idea", "Beginning Idea", "Interesting Idea", "Smart Idea", "Genius Idea", "What an Idea!"]
-URLS = ['https://robohash.org/a', 'https://robohash.org/b', 'https://robohash.org/c', 'https://robohash.org/d',
-        'https://robohash.org/e', 'https://robohash.org/f', 'https://robohash.org/g', 'https://robohash.org/h',
-        'https://robohash.org/i', 'https://robohash.org/j', 'https://robohash.org/k', 'https://robohash.org/l',
-        'https://robohash.org/m', 'https://robohash.org/n', 'https://robohash.org/o', 'https://robohash.org/p',
-        'https://robohash.org/q', 'https://robohash.org/r', 'https://robohash.org/s', 'https://robohash.org/t']
+CATEGORIES  = ["Red", "Orange", "Green", "Yellow", "Blue", "Indigo", "Violet",
+              "Pink", "Black", "Brown", "Magenta", "Pearl", "Aero", "Aero Blue",
+              "Almond", "Aluminum", "Amber", "Gold", "Silver",
+              "Ao", "Apple", "Aqua", "Lime", "Lemon", "Apple", "Azure"]
 
+IDEAS       = ["Peace", "Love", "Understanding", "Haromony", "Music", "Friendship", "Family", "Community"]
+
+DESCRIPTION = ["Perfect Idea", "Wonderfull Idea", "Clever Idea", "Meaningful Idea",
+               "Productive Idea", "Unique Idea", "First Idea", "Last Idea", "Beginning Idea",
+               "Interesting Idea", "Smart Idea", "Genius Idea", "What an Idea!"]
 
 CATEGORIES.each do |title|
   category = Category.create!(title: title)
   puts "Created #{category.title}"
   idea = category.ideas.create!(title: IDEAS.sample, description: DESCRIPTION.sample, user: admin)
-  idea.images.create!(url: URLS.sample, title: CATEGORIES.sample)
+  idea.images.create!(url: IMAGEURLS.sample, title: CATEGORIES.sample)
 end
 
 CATEGORIES.each do |title|
   category = Category.create!(title: title)
   puts "Created #{category.title}"
   idea = category.ideas.create!(title: IDEAS.sample, description: DESCRIPTION.sample, user: user)
-  idea.images.create!(url: URLS.sample, title: CATEGORIES.sample)
+  idea.images.create!(url: IMAGEURLS.sample, title: CATEGORIES.sample)
 end
