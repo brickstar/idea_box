43e653ab60a2e8cd664ec17e36d6677b6da3db7e
brickstar <brickstar@gmail.com>
2018-07-18 10:59:13 -0600

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,16 +7,15 @@ admin = User.create(username: 'Pearl', password: 'love', role: 1)
 user  = User.create(username: 'Heidi', password: 'love', role: 0)
 
 IMAGEURLS = []
-1000.times do
+100.times do
   IMAGEURLS << "https://robohash.org/#{rand(1000)}"
 end
 
 CATEGORIES  = ["Red", "Orange", "Green", "Yellow", "Blue", "Indigo", "Violet",
               "Pink", "Black", "Brown", "Magenta", "Pearl", "Aero", "Aero Blue",
-              "Almond", "Aluminum", "Amber", "Gold", "Silver",
-              "Ao", "Apple", "Aqua", "Lime", "Lemon", "Apple", "Azure"]
+              "Almond", "Aqua", "Amber", "Gold", "Silver"]
 
-IDEAS       = ["Peace", "Love", "Understanding", "Haromony", "Music", "Friendship", "Family", "Community"]
+TITLES      = ["Peace", "Love", "Understanding", "Haromony", "Music", "Friendship", "Family", "Community"]
 
 DESCRIPTION = ["Perfect Idea", "Wonderfull Idea", "Clever Idea", "Meaningful Idea",
                "Productive Idea", "Unique Idea", "First Idea", "Last Idea", "Beginning Idea",
@@ -25,13 +24,17 @@ DESCRIPTION = ["Perfect Idea", "Wonderfull Idea", "Clever Idea", "Meaningful Ide
 CATEGORIES.each do |title|
   category = Category.create!(title: title)
   puts "Created #{category.title}"
-  idea = category.ideas.create!(title: IDEAS.sample, description: DESCRIPTION.sample, user: admin)
+  idea = category.ideas.create!(title: TITLES.sample, description: DESCRIPTION.sample, user: admin)
   idea.images.create!(url: IMAGEURLS.sample, title: CATEGORIES.sample)
 end
 
 CATEGORIES.each do |title|
   category = Category.create!(title: title)
   puts "Created #{category.title}"
-  idea = category.ideas.create!(title: IDEAS.sample, description: DESCRIPTION.sample, user: user)
+  idea = category.ideas.create!(title: TITLES.sample, description: DESCRIPTION.sample, user: user)
   idea.images.create!(url: IMAGEURLS.sample, title: CATEGORIES.sample)
+end
+
+12.times do
+  Image.create(url: IMAGEURLS.sample, title: CATEGORIES.sample)
 end
